5a82019041ff03278a87996f81bc1c955e2ee7eb
jirentianxiang <jirenpersonalmail@gmail.com>
2019-08-25 18:30:28 +0800

    重构函数名

--- crawler.py
+++ crawler.py
@@ -1,31 +1,34 @@
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
 
-def get_img_url():
+def img_url():
     soup = urlopen('https://octodex.github.com')
     content = BeautifulSoup(soup.read(), 'html.parser')
     img_urls = []
-    for link in content.find_all('img'):
+    for link in content.find_all('img'):        
         if link.get('data-src') != None:
             img_urls.append('https://octodex.github.com' + link.get('data-src'))
     return img_urls
 
-def get_img_name():
+def img_name():
     soup = urlopen('https://octodex.github.com')
     content = BeautifulSoup(soup.read(), 'html.parser')
     img_names = []
     for link in content.find_all('a'):
-        if link.get('name') != None:
-            img_names.append(link.get('name'))
+        if link.text != None:
+            img_names.append(link.text)
+    print(img_names)
     return img_names
 
 def img_download():
-    img_urls = get_img_url()
-    img_names = get_img_name()
-    i = 0
-    for url in img_urls:
-        urlretrieve(url, './Octodex/' + img_names[i] + '.jpg')
-        i += 1
+        i = 0
+#     img_urls = img_url()
+#     img_names = img_name()
+#     i = 0
+#     for url in img_urls:
+#         urlretrieve(url, './Octodex/' + img_names[i] + '.jpg')
+#         i += 1
 
 if __name__ == '__main__':
-    img_download()
+#     img_download()
+   img_name()
